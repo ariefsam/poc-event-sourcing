0c75f6326951e59498f25f43457596cdb26ebb47
Arief Hidayatulloh <ariefhidayatulloh@gmail.com>
2020-10-21 18:39:13 +0700

    refactor mock events

--- event-service/handler/EventServiceMock_test.go
+++ event-service/handler/EventServiceMock_test.go
@@ -20,46 +20,9 @@ func (m EventServiceMock) ReadAfter(afterID string) (event []map[string]interfac
 	return
 }
 
-func GetEventServiceMock() (mockEventService EventServiceMock) {
+func GetEventServiceMock(events []map[string]interface{}) (mockEventService EventServiceMock) {
 	mockEventService.On("Write", mock.MatchedBy(func(e map[string]interface{}) bool { return true })).Return("theID", nil)
-	events := []map[string]interface{}{
-		map[string]interface{}{
-			"EventName": "UserCreated",
-			"Payload": map[string]interface{}{
-				"ID":    "j10",
-				"Name":  "Arief Hidayatulloh",
-				"Phone": "0852163772",
-				"Email": "arief.hidayatulloh@gmail.com",
-				"Role":  "User",
-				"CreatedBy": map[string]interface{}{
-					"ID":    "j1",
-					"Name":  "Nam Indra",
-					"Email": "nam@jojonomic.com",
-					"Role":  "Admin",
-				},
-				"CreatedAtTimestamp": 1603235393,
-			},
-			"SystemUnixNano": 1603235393816625000,
-		},
-		map[string]interface{}{
-			"EventName": "UserUpdated",
-			"Payload": map[string]interface{}{
-				"ID":    "j10",
-				"Name":  "Arief Hidayatulloh S.Komp",
-				"Phone": "085212345678",
-				"Email": "arief.hidayatulloh@gmail.com",
-				"Role":  "User",
-				"UpdatedBy": map[string]interface{}{
-					"ID":    "j1",
-					"Name":  "Nam Indra",
-					"Email": "nam@jojonomic.com",
-					"Role":  "Admin",
-				},
-				"CreatedAtTimestamp": 1603235414,
-			},
-			"SystemUnixNano": 1603235414254786000,
-		},
-	}
+
 	mockEventService.On("ReadAfter", mock.MatchedBy(func(id string) bool { return true })).Return(events, nil)
 	return
 }
--- event-service/handler/WriteHandler_test.go
+++ event-service/handler/WriteHandler_test.go
@@ -30,13 +30,51 @@ func TestWriteHandler(t *testing.T) {
 			},
 		},
 	}
+	readEvents := []map[string]interface{}{
+		map[string]interface{}{
+			"EventName": "UserCreated",
+			"Payload": map[string]interface{}{
+				"ID":    "j10",
+				"Name":  "Arief Hidayatulloh",
+				"Phone": "0852163772",
+				"Email": "arief.hidayatulloh@gmail.com",
+				"Role":  "User",
+				"CreatedBy": map[string]interface{}{
+					"ID":    "j1",
+					"Name":  "Nam Indra",
+					"Email": "nam@jojonomic.com",
+					"Role":  "Admin",
+				},
+				"CreatedAtTimestamp": 1603235393,
+			},
+			"SystemUnixNano": 1603235393816625000,
+		},
+		map[string]interface{}{
+			"EventName": "UserUpdated",
+			"Payload": map[string]interface{}{
+				"ID":    "j10",
+				"Name":  "Arief Hidayatulloh S.Komp",
+				"Phone": "085212345678",
+				"Email": "arief.hidayatulloh@gmail.com",
+				"Role":  "User",
+				"UpdatedBy": map[string]interface{}{
+					"ID":    "j1",
+					"Name":  "Nam Indra",
+					"Email": "nam@jojonomic.com",
+					"Role":  "Admin",
+				},
+				"CreatedAtTimestamp": 1603235414,
+			},
+			"SystemUnixNano": 1603235414254786000,
+		},
+	}
 
 	requestJson, _ := json.Marshal(event)
 
 	r := httptest.NewRequest("GET", "http://xx/x", bytes.NewReader(requestJson))
 	w := httptest.NewRecorder()
 
-	mockEventService := GetEventServiceMock()
+	mockEventService := GetEventServiceMock(readEvents)
 	h := handler.HttpHandler{
 		EventService: &mockEventService,
 	}
